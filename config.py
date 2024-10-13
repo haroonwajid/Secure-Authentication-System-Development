c2c8e7634a0c73721314efbcebac0c1dd81bb0f9
Haroon Wajid <130307173+haroonwajid@users.noreply.github.com>
2024-10-14 01:53:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'haroonkey'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_USERNAME = 'haroonwajid590@gmail.com'
-    MAIL_PASSWORD = 'sqvz ttsc lpdn abay'
+    MAIL_USERNAME = 'replace with email'
+    MAIL_PASSWORD = 'replace with app password'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
